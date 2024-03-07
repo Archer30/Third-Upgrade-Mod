94687b104c54c9409c140282ceeb7d1b751eb184
Diozia <rockylamaestra@gmail.com>
2024-03-07 14:53:54 +0100

    Commit for TUM Lite Enchanted 1.2 Release
    CHANGELOG TUM LITE ENCHANTED EDITION 1.2
    
    BUG FIXES:
    *DIAMOND DRAGON Hero spell point regeneration ability removed (Dling Dlong Sound) thanks to SadnessPower.
    *Ogre Magi can now cast Haste correctly.
    *Might of the Catoblepas Bonus is now show correclty (+4 def).
    *Mummy description show 100% cursed attack.
    
    BALANCE:
    *GENERAL: All active buff/debuff caster units now are more useful with a basic +3 Spell Power, then if you decide to invest 1 creature turn to cast, your buff/debuff duration is at least 4 turns.
    
    *PREATORIANS "Wall of Shield Formation" bonus now grants -1 Atk +6 Def instead of -1 Atk +4 Def.
    *GOLD DRAGON "Dragon Defence" bonus now grants +4 Def instead of -1 Atk +4 Def, but chance are lowered to 20% instead of 30.
    *DIAMOND DRAGON "Ultimate Defence" bonus now grants +8 Def
    instead of +6 Def, but chance are lowered to 30% instead of 40%.
    *BATTLEMAGE OF THE CITY OF ICE AND STEEL "Parry" chance is increased to 30% instead of 20% and have +1 Defence skill.
    *ASSASSIN OF THE GOLDEN CITY defence skill is increased to 16 instead of 15.
    *MASTER OF THE SIX BLADES defence skill is increased to 18 instead of 16.
    *IMP now can actively cast Expert Misfortune instead of Misfortune.
    *HELL BARON "Strong Diabolic Fury" chance are lowered to 30% instead of 40%.
    *BLOOD DRAGON damage is lowered to 30-60 instead of 35-65. Attack and Defence skills are now -1.
    *LORD OF THE SWAMP defence skill is lowered to 22 instead of 24. Price is lowered by -100 gold.
    *SACRED PHOENIX defence and attack skill is lowered by -2 either.

--- MakeJunction.bat
+++ MakeJunction.bat
@@ -1,3 +1,3 @@
-mklink /j "C:\Games\Heroes 3 ERA 3\Mods\TUM Enchanted" "C:\Games\Heroes 3 ERA 3\Repository\TUM Enchanted"
+mklink /j "C:\Games\Heroes 3 ERA 3\Mods\TUM Lite Enchanted" "C:\Games\Heroes 3 ERA 3\Repository\TUM Lite Enchanted"
 mklink /j "C:\Games\Heroes 3 ERA 3\Mods\TUM Lite" "C:\Games\Heroes 3 ERA 3\Repository\TUM Lite"
 
